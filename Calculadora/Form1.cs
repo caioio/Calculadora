12334d3216a567a19e1b945d41d6820abb60c47a
CaioBurns <caioburns@ucl.br>
2021-11-04 20:58:38 -0300

    Arquivos iniciais

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcEvent(object sender, EventArgs e)
+        {
+            tBmensagem.Text = " ";
+            try
+            {
+                //double result = (double.Parse(textBox2.Text) + double.Parse(textBox1.Text));
+                textBox3.Text = (double.Parse(textBox2.Text) + double.Parse(textBox1.Text)).ToString();
+            }
+            catch (Exception ex)
+            {
+                tBmensagem.Text = ex.ToString();
+            }
+        }
+    }
+}
